8b8b842eb173fdc8711b5dc43acb26a7216e9d9b
vachagunn <vachikpuper@gmail.com>
2024-07-16 14:58:35 +0500

    nested layout

---
+++ app/about/layout.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+export default function AboutLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return <div>
+        <h1>About us</h1>
+        <ul>
+            <li><Link href="/about/contacts">Contacts</Link></li>
+            <li><Link href="/about/team">Team</Link></li>
+        </ul>
+        {/* children - is a page */}
+        {children}
+    </div>
+}
